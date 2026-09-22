5384f7aa0786e13f66f5e823d6a5c0fba603302f
Shaik Tanveer Basha <tanveerbasha1322@gmail.com>
2026-09-22 23:20:17 +0530

    Book Services Updated

---
+++ book-service/auth-service/src/main/java/com/bibliotech/auth_service/service/JwtService.java
@@ -0,0 +1,46 @@
+package com.bibliotech.auth_service.service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    private final SecretKey key;
+
+    public JwtService(@Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+    
+    //BibliotechSecretKeyForJwtAuthentication2026Secure
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()
+                        + 1000 * 60 * 60))
+                .signWith(key)
+                .compact();
+    }
+    
+    public String validateToken(String token) {
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
